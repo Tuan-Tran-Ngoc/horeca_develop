c44c88660485afbbe63c80fd69f97ed1e7cf53f7
kanquockien <kanquockien@gmail.com>
2026-01-22 16:12:29 +0700

    Update network.dart and add horeca2.apk for release traceability

--- package/horeca_service/lib/contants/network.dart
+++ package/horeca_service/lib/contants/network.dart
@@ -4,5 +4,6 @@ class Network {
   //static const String url = 'http://192.168.24.27:8083/horeca';
   // static const String url = 'http://192.168.24.27:8083/horeca_test';
   // static const String url = 'https://www1.acecookvietnam.vn/horecatraining';
-  static const String url = 'https://www1.acecookvietnam.vn/horeca_test';
+  static const String url =
+      'https://www1.acecookvietnam.vn/horecaapp-arm64-v8a-release.apk ';
 }
